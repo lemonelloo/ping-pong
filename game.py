debeb256876aa814b73d37675486673c19a851b1
lemonelloo <129597691+lemonelloo@users.noreply.github.com>
2023-04-08 14:51:34 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,20 @@
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (85,85))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.speed
