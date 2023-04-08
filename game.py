0fcad1de50c581e670d272147d50a04291a86a86
lemonelloo <129597691+lemonelloo@users.noreply.github.com>
2023-04-08 15:29:35 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from pygame import *
+
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (85,85))
         self.speed = player_speed
@@ -11,6 +12,47 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 420:
+            self.rect.y += self.speed
+
+back = (200,255,255)
+window = display.set_mode((600,500))
+window.fill(back)
+
+racket1 = Player('racket.png', 20,200,4,50,150)
+racket2 = Player('racket.png', 500,200,4,20,150)
+
+game = True
+finish = False
+
+clock = time.Clock()
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if not finish:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+
+    display.update()
+    clock.tick(50)
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
